bf78bf6950adee612b4811304accef478c4e75bc
Martin Mander <mwmander@gmail.com>
2016-07-18 08:26:04 +0100

    Add files via upload

---
+++ Convert.py
@@ -0,0 +1,37 @@
+import os
+import time
+
+print 'Converting video 01. Please wait...'
+os.system("MP4Box -add bat0001.h264  bat0001.mp4")
+print 'Converting video 02. Please wait...'
+os.system("MP4Box -add bat0002.h264  bat0002.mp4")
+print 'Converting video  03. Please wait...'
+os.system("MP4Box -add bat0003.h264  bat0003.mp4")
+print 'Converting video 04. Please wait...'
+os.system("MP4Box -add bat0004.h264  bat0004.mp4")
+print 'Converting video 05. Please wait...'
+os.system("MP4Box -add bat0005.h264  bat0005.mp4")
+print 'Converting video 06. Please wait...'
+os.system("MP4Box -add bat0006.h264  bat0006.mp4")
+print 'Converting video 07. Please wait...'
+os.system("MP4Box -add bat0007.h264  bat0007.mp4")
+print 'Converting video  08. Please wait...'
+os.system("MP4Box -add bat0008.h264  bat0008.mp4")
+print 'Converting video 09. Please wait...'
+os.system("MP4Box -add bat0009.h264  bat0009.mp4")
+print 'Converting video 10. Please wait...'
+os.system("MP4Box -add bat0010.h264  bat0010.mp4")
+print 'Converting video 11. Please wait...'
+os.system("MP4Box -add bat0011.h264  bat0011.mp4")
+print 'Converting video 12. Please wait...'
+os.system("MP4Box -add bat0012.h264  bat0012.mp4")
+print 'Converting video  13. Please wait...'
+os.system("MP4Box -add bat0013.h264  bat0013.mp4")
+print 'Converting video 14. Please wait...'
+os.system("MP4Box -add bat0014.h264  bat0014.mp4")
+
+print 'Video conversion complete'
+time.sleep(2)
+ 
+print 'Closing programme'
+time.sleep(2)
